279d85691cb238ec28ad8d2e5afd47ec9d60ac01
Prila1 <yarteypriscy@gmail.com>
2023-12-26 13:43:56 -0800

    1-install_a_package.pp

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+# Downloads the packag
+
+package {'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  before   => Package['Flask']
+}
